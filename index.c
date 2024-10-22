b359e94be4b3ad4ed1adb83e6072a361e1490dfd
sdishehab <150367787+shehab097@users.noreply.github.com>
2024-10-22 16:57:54 +0600

    first commit

---
+++ index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct index{
+    
+};
+
+
+int main(void) {
+    
+    
+    return 0;
+}
