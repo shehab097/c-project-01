5daa9bfd7e642aa19fdafc3ab4f9cad23d8c0b38
sdishehab <150367787+shehab097@users.noreply.github.com>
2024-10-30 20:44:03 +0600

    "updated all"

--- note.c
+++ note.c
@@ -1,18 +1,13 @@
-typedef struct subject_data {
-    ////  "Bangla","English","Math","ICT", "Science" };
+typedef struct student_data {
+    // basic info - personal info
+    char    name[50];
+    char    id[10];
+    // float   gpa;
+    // char    grade[5]; //A+ ,A,A-,B,C,D,F
+    char    date_of_birth[10]; //dd:mm:yy
+
+    //subject info
     int     marks[TOTAL_SUBJCT];    //all marks of each student
     float   gpa[TOTAL_SUBJCT];      //all gpa of each student
     char    grade[TOTAL_SUBJCT][3]; //All grade list of each student
-}subject_data;
-
-
-
-// basic info - personal info
-typedef struct student_data {
-    char    name[50];
-    char    id[10];
-    float   gpa;
-    char    grade[5]; //A+ ,A,A-,B,C,D,F
-    char    date_of_birth[10]; //dd:mm:yy
-    subject_data subjects; //linked with //* subject_data
-}student
+}student;
